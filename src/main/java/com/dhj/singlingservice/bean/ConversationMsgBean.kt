8a1f47278d912242fd30dceee44caef3028461df
dahj <dhj2318243304>
2023-06-02 15:44:09 +0800

    增加数据库

---
+++ src/main/java/com/dhj/singlingservice/bean/ConversationMsgBean.kt
@@ -0,0 +1,8 @@
+package com.dhj.singlingservice.bean
+
+class ConversationMsgBean: MsgBean(){
+    var time: String = ""
+    var fromId: String = ""
+    var fromName: String = ""
+    var msgContent: String = ""
+}
